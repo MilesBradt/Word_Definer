f7967fa2309e2f1c26a364229fa1506ce465e2cf
Miles Bradt <smbradtmichael@gmail.com>
2018-11-30 12:45:19 -0800

    update tests to include definition checks

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -5,31 +5,49 @@ describe("find_by_id") do
   it("should return the first test word based on its ID") do
 
   test_word = Word.new({:word => "Ruby", :definition => "Programming language"})
-
   test_word.save
 
   test_word2 = Word.new({:word => "Ruby (2)", :definition => "Programming language (2)"})
-
   test_word2.save
 
   test_method_first_word = Word.find_by_id(1)
-
   expect(test_method_first_word.word).to(eq("Ruby"))
   end
 
   it("should return the second test word based on its ID") do
 
   test_word = Word.new({:word => "Ruby", :definition => "Programming language"})
-
   test_word.save
 
   test_word2 = Word.new({:word => "Ruby (2)", :definition => "Programming language (2)"})
-
   test_word2.save
 
   test_method_second_word = Word.find_by_id(2)
-
   expect(test_method_second_word.word).to(eq("Ruby (2)"))
+  end
+
+  it("should return the first test definition based on its ID") do
+
+  test_def = Word.new({:word => "Ruby", :definition => "Programming language"})
+  test_def.save
+
+  test_def = Word.new({:word => "Ruby (2)", :definition => "Programming language (2)"})
+  test_def.save
+
+  test_method_first_def = Word.find_by_id(1)
+  expect(test_method_first_def.definition).to(eq("Programming language"))
+  end
+
+  it("should return the second test definition based on its ID") do
+
+  test_word = Word.new({:word => "Ruby", :definition => "Programming language"})
+  test_word.save
+
+  test_word2 = Word.new({:word => "Ruby (2)", :definition => "Programming language (2)"})
+  test_word2.save
+
+  test_method_second_def = Word.find_by_id(2)
+  expect(test_method_second_def.definition).to(eq("Programming language (2)"))
   end
 
 end
