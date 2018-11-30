cadc4fa0ec504a591eae57dff72ad5e0994cdc73
Miles Bradt <smbradtmichael@gmail.com>
2018-11-30 12:21:05 -0800

    assign ID number to each word

--- lib/word.rb
+++ lib/word.rb
@@ -1,13 +1,13 @@
 class Word
 
-  attr_accessor :word, :definition
+  attr_accessor :word, :definition, :id
 
   @@words = []
 
   def initialize(attributes)
     @word = attributes.fetch(:word)
     @definition = attributes.fetch(:definition)
-    @id = @@words.length
+    @id = @@words.length + 1
   end
 
   def save
@@ -17,4 +17,18 @@ class Word
   def self.view_all
     return @@words
   end
+
+  def self.find_by_id(id)
+    word_id = id.to_i()
+    @@words.each do |word|
+      if word.id == word_id
+        return word
+      end
+    end
+  end
+
 end
+
+test = Word.new({:word => "Ruby", :definition => "Programming language"})
+
+test2 = Word.new({:word => "Ruby (2)", :definition => "Programming language (2)"})
