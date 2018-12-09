0e371577f380283aaa666cec1b234e269852e853
Miles Bradt <smbradtmichael@gmail.com>
2018-12-08 19:05:09 -0800

    remove not needed testing specs

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -10,35 +10,6 @@ describe Word do
 
     expect(Word.find_by_id(1).word).to(eq("Ruby"))
     end
-
-    it("should return the second test word based on its ID") do
-    test_word = Word.new({:word => "Ruby", :definition => "Programming language"})
-    test_word.save
-    test_word2 = Word.new({:word => "Ruby (2)", :definition => "Programming language (2)"})
-    test_word2.save
-
-    expect(Word.find_by_id(2).word).to(eq("Ruby (2)"))
-    end
-
-    it("should return the first test definition based on its ID") do
-    test_def = Word.new({:word => "Ruby", :definition => "Programming language"})
-    test_def.save
-    test_def = Word.new({:word => "Ruby (2)", :definition => "Programming language (2)"})
-    test_def.save
-
-    expect(Word.find_by_id(1).definition).to(eq("Programming language"))
-    end
-
-    it("should return the second test definition based on its ID") do
-    test_word = Word.new({:word => "Ruby", :definition => "Programming language"})
-    test_word.save
-    test_word2 = Word.new({:word => "Ruby (2)", :definition => "Programming language (2)"})
-    test_word2.save
-
-    expect(Word.find_by_id(2).definition).to(eq("Programming language (2)"))
-    end
   end
-
-
-
+  
 end
